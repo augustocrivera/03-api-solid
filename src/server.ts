23db98652644d52a1cd0be1d1c6ce3722d5578da
Augusto <augustoalfafa@gmail.com>
2023-09-04 10:23:23 -0300

    chore: initial project config

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
-import { app } from "./app";
+import { env } from './env'
+import { app } from './app'
 
-app.listen({host: '0.0.0.0',port: 3333}).then(() => console.log('🚀 Server is running!'))
+app
+  .listen({ host: '0.0.0.0', port: env.PORT })
+  .then(() => console.log('🚀 Server is running!'))
